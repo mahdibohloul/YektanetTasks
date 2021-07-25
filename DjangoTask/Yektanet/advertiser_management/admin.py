5ee343fbf619cd3b38b983035013fec090c391ee
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-25 18:32:06 +0430

    advertisers and ad admin view customized.

--- DjangoTask/Yektanet/advertiser_management/admin.py
+++ DjangoTask/Yektanet/advertiser_management/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Advertiser, Ad
 
-admin.site.register(Ad)
-admin.site.register(Advertiser)
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['title', 'clicks', 'views', 'advertiser']
+    readonly_fields = ['clicks', 'views']
+    search_fields = ['title', 'advertiser__name']
+    ordering = ['-clicks', ]
+
+
+class AdTabular(admin.TabularInline):
+    model = Ad
+    readonly_fields = ['views', 'clicks']
+
+
+@admin.register(Advertiser)
+class AdvertiserAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'ads__title']
+    list_display = ['name', 'clicks', 'views']
+    readonly_fields = ['clicks', 'views']
+    inlines = [AdTabular, ]
+    ordering = ['-clicks', ]
