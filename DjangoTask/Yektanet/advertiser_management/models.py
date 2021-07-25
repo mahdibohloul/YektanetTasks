23590e6de18a879fb0bfd636286107f947d6e5f4
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-25 14:51:03 +0430

    advertiser and ad models added

--- DjangoTask/Yektanet/advertiser_management/models.py
+++ DjangoTask/Yektanet/advertiser_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertiser(models.Model):
+    name = models.CharField(max_length=50)
+    clicks = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=50)
+    img_url = models.URLField()
+    link = models.URLField()
+    views = models.PositiveIntegerField(default=0)
+    clicks = models.PositiveIntegerField(default=0)
+
