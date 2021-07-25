1120f60372991ef48e2e4c88ea4b3495f9a56367
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-25 14:50:41 +0430

    settings installed app configured

--- DjangoTask/Yektanet/Yektanet/settings.py
+++ DjangoTask/Yektanet/Yektanet/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CREATED_APPS = [
+    'advertiser_management.apps.AdvertiserManagementConfig',
+]
+
+THIRD_PARTY_APPS = [
+
+]
+
+INSTALLED_APPS = DEFAULT_DJANGO_APPS + CREATED_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
