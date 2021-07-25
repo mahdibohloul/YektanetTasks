6e43eed6ce074737a5aa5fa6f4b91b04c57627aa
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-25 20:39:03 +0430

    media configuration is added

--- DjangoTask/Yektanet/advertiser_management/urls.py
+++ DjangoTask/Yektanet/advertiser_management/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.ad_list, name='ads-list'),
     path('click/<slug:pk>', views.ClickAdView.as_view(), name='click-ad-view'),
+    path('create-form', views.CreateAdView.as_view(), name='create-ad'),
 ]
