f338e9d2a493af5b6e0ed89e6f19529effa65e56
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-26 13:49:36 +0430

    change ad list view from fbw to cbw

--- DjangoTask/Yektanet/advertiser_management/urls.py
+++ DjangoTask/Yektanet/advertiser_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ad_list, name='ads-list'),
+    path('', views.AdListView.as_view(), name='ads-list'),
     path('click/<slug:pk>', views.ClickAdView.as_view(), name='click-ad-view'),
     path('create-ad', views.CreateAdView.as_view(), name='create-ad'),
     path('create-adviser', views.CreateAdvertiserView.as_view(), name='create-adviser'),
--- DjangoTask/Yektanet/advertiser_management/views.py
+++ DjangoTask/Yektanet/advertiser_management/views.py
@@ -1,21 +1,25 @@
 from django.contrib import messages
-from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import RedirectView, CreateView
+from django.views.generic import RedirectView, CreateView, TemplateView
 
 from .forms import CreateAdForm, CreateAdvertiserForm
 from .models import Advertiser
 from .services import get_ad_by_pk, update_advertisers_views
 
 
-def ad_list(request):
-    advertisers = Advertiser.objects.all()
-    update_advertisers_views(advertisers)
+class AdListView(TemplateView):
+    template_name = 'advertiser_management/ads.html'
 
-    context = {
-        'advertisers': advertisers
-    }
-    return render(request, 'advertiser_management/ads.html', context=context)
+    def get_context_data(self, **kwargs):
+        advertisers = Advertiser.objects.all()
+        print(self.request.ip_addr)
+        update_advertisers_views(advertisers, self.request.ip_addr)
+
+        context = {
+            'advertisers': advertisers
+        }
+
+        return context
 
 
 class ClickAdView(RedirectView):
@@ -25,7 +29,7 @@ class ClickAdView(RedirectView):
         if ad is None:
             messages.error(self.request, 'Could not find any Ad with this specific identifier')
             return reverse('ads-list')
-        ad.inc_clicks()
+        ad.inc_clicks(self.request.ip_addr)
         return ad.link
 
 
