7d0ded8167176ca891a37fece384a1605fe4db82
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-26 13:48:45 +0430

    ip caching middleware implemented

--- DjangoTask/Yektanet/Yektanet/settings.py
+++ DjangoTask/Yektanet/Yektanet/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'advertiser_management.middlewares.IpAddressMiddleware',
 ]
 
 ROOT_URLCONF = 'Yektanet.urls'
