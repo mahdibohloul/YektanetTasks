f66cc9b0e6ea5e0729c19a2f77979d0640583fc0
Mahdi Bohloul <mahdi_bohloul@yahoo.com>
2021-07-28 11:58:33 +0430

    user api url transferred to 'api' base rout

--- DjangoTask/Yektanet/users/urls.py
+++ DjangoTask/Yektanet/users/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('', views.UserViewSet, basename='users')
+router.register('api', views.UserViewSet, basename='users')
 
 urlpatterns = router.urls
